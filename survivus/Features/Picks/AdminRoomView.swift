4724788f90d0442f3544866b8b7c55ed4416ad73
1zxchxrix <zachariasalad@icloud.com>
2025-10-05 10:01:55 -0500

    Update admin room layout

--- survivus/Features/Picks/AdminRoomView.swift
+++ survivus/Features/Picks/AdminRoomView.swift
@@ -2,11 +2,27 @@ import SwiftUI
 
 struct AdminRoomView: View {
     var body: some View {
-        Text("Admin Room")
-            .font(.title)
-            .fontWeight(.semibold)
-            .padding()
-            .navigationTitle("Admin Room")
+        Form {
+            Section {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Current Phase: Pre-merge")
+                    Text("Current Week: 2")
+                }
+            }
+
+            Section("Week") {
+                Button("Insert Results") {}
+                Button("Modify Previous Results") {}
+                Button("Start New Week") {}
+            }
+
+            Section("Phase") {
+                Button("Select Current Phase") {}
+                Button("Create New Phase") {}
+                Button("Remove Phase") {}
+            }
+        }
+        .navigationTitle("Admin Room")
     }
 }
 
