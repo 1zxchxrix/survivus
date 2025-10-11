58d5a02134bd17329a3ddbac843e992b9a7a0263
Zac Salad <zachariasalad@icloud.com>
2025-10-11 12:43:25 -0500

    added firestore access

--- survivus/survivusApp.swift
+++ survivus/survivusApp.swift
@@ -1,9 +1,18 @@
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct SurvivusApp: App {
     @StateObject private var app = AppState()
-
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             RootView()
